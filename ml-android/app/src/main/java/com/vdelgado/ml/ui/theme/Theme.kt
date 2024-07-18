44214420ee446595e945a2cf30dd84d9e7c1efbb
vdelgado <vinnygarciadelgado@gmail.com>
2024-07-18 18:41:04 -0300

    🏗️ [ML][2] Initial Kotlin DSL Setup

--- ml-android/app/src/main/java/com/vdelgado/ml/ui/theme/Theme.kt
+++ ml-android/app/src/main/java/com/vdelgado/ml/ui/theme/Theme.kt
@@ -35,8 +35,7 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun MLTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
