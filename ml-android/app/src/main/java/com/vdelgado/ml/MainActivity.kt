aceb0b18b5ba1236ee0c032358688c52dc37d9e7
vdelgado <vinnygarciadelgado@gmail.com>
2024-07-20 19:53:01 -0300

    ✨ Error Screen

--- ml-android/app/src/main/java/com/vdelgado/ml/MainActivity.kt
+++ ml-android/app/src/main/java/com/vdelgado/ml/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -32,6 +36,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
@@ -39,6 +47,7 @@ import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.vdelgado.ml.domain.model.MLProductFormatted
 import com.vdelgado.ml.presentation.HomeViewModel
+import com.vdelgado.ml.presentation.ProductItem
 import com.vdelgado.ml.ui.theme.MLTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -104,7 +113,7 @@ class MainActivity : ComponentActivity() {
                         if (products.loadState.refresh is LoadState.Loading) {
                             ProgressLoading()
                         } else if (products.loadState.refresh is LoadState.Error) {
-                            Text("Erro")
+                            ErrorUser()
                         } else {
                             ProductList(
                                 products = products, modifier = Modifier
@@ -121,11 +130,48 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ProgressLoading() {
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         CircularProgressIndicator(
             modifier = Modifier.width(64.dp),
             color = MaterialTheme.colorScheme.secondary,
             trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    }
+}
+
+@Composable
+fun ErrorUser(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier.size(76.dp),
+            imageVector = ImageVector.vectorResource(R.drawable.ic_error_icon),
+            contentDescription = "Erro"
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text ="Parece que tivemos um erro!",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.Light
+        )
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Nossa equipe já está trabalhando para resolver o problema, tente novamente dentro de alguns minutos",
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Light,
         )
     }
 }
