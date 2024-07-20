4cd3003b56a9eed20cdf42403351cafe60ed8471
vdelgado <vinnygarciadelgado@gmail.com>
2024-07-20 19:21:35 -0300

    ✨ Create loading screen

--- ml-android/app/src/main/java/com/vdelgado/ml/MainActivity.kt
+++ ml-android/app/src/main/java/com/vdelgado/ml/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.vdelgado.ml
 
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
@@ -25,10 +30,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.vdelgado.ml.domain.model.MLProductFormatted
@@ -54,7 +61,7 @@ class MainActivity : ComponentActivity() {
                             text = it
                         },
                         onSearch = {
-                            if(active && text.isNotEmpty()){
+                            if (active && text.isNotEmpty()) {
                                 viewModel.searchProductPager(text)
                             }
                             active = false
@@ -95,11 +102,17 @@ class MainActivity : ComponentActivity() {
                 }, modifier = Modifier.fillMaxSize()) { innerPadding ->
                     viewModel.state.value.products?.let { items ->
                         val products = items.collectAsLazyPagingItems()
-                        ProductList(
-                            products = products, modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(innerPadding)
-                        )
+                        if (products.loadState.refresh is LoadState.Loading) {
+                            ProgressLoading()
+                        } else if (products.loadState.refresh is LoadState.Error) {
+                            Text("Erro")
+                        } else {
+                            ProductList(
+                                products = products, modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(innerPadding)
+                            )
+                        }
                     }
                 }
             }
@@ -107,6 +120,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun ProgressLoading() {
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(64.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    }
+}
 
 @Composable
 fun ProductList(modifier: Modifier = Modifier, products: LazyPagingItems<MLProductFormatted>) {
