dcd5b76102835c179689e7cce9af49013f9aea8f
vdelgado <vinnygarciadelgado@gmail.com>
2024-07-20 18:44:00 -0300

    ⬆️ 🚚 Change files to package correct

--- ml-android/app/src/main/java/com/vdelgado/ml/data/remote/MLSearchProductResponse.kt
+++ ml-android/app/src/main/java/com/vdelgado/ml/data/remote/MLSearchProductResponse.kt
@@ -1,31 +1,31 @@
-package com.vdelgado.ml.domain.model
+package com.vdelgado.ml.data.remote
 
 import com.google.gson.annotations.SerializedName
 
-data class MLSearchProduct(
+data class MLSearchProductResponse(
     @SerializedName("site_id") val siteId: String,
     @SerializedName("country_default_time_zone") val countryDefaultTimeZone: String,
     @SerializedName("query") val query: String,
-    @SerializedName("paging") val paging: MLPaging,
-    @SerializedName("results") val results: List<MLProduct>,
-    @SerializedName("sort") val sort: MLSort,
-    @SerializedName("available_sorts") val availableSorts: List<MLSort>,
-    @SerializedName("filters") val filters: List<MLFilter>,
+    @SerializedName("paging") val paging: MLPagingResponse,
+    @SerializedName("results") val results: List<MLProductResponse>,
+    @SerializedName("sort") val sort: MLSortResponse,
+    @SerializedName("available_sorts") val availableSorts: List<MLSortResponse>,
+    @SerializedName("filters") val filters: List<MLFilterResponse>,
     @SerializedName("available_filters") val availableFilters: List<MLAvailableFilter>
 )
 
-data class MLPaging(
+data class MLPagingResponse(
     @SerializedName("total") val total: Int,
     @SerializedName("primary_results") val primaryResults: Int,
     @SerializedName("offset") val offset: Int,
     @SerializedName("limit") val limit: Int
 )
 
-data class MLProduct(
-    @SerializedName("id") val id: String?,
-    @SerializedName("title") val title: String?,
-    @SerializedName("condition") val condition: String?,
-    @SerializedName("thumbnail_id") val thumbnailId: String?,
+data class MLProductResponse(
+    @SerializedName("id") val id: String,
+    @SerializedName("title") val title: String,
+    @SerializedName("condition") val condition: String,
+    @SerializedName("thumbnail_id") val thumbnailId: String,
 //    @SerializedName("catalog_product_id") val catalogProductId: String?,
 //    @SerializedName("listing_type_id") val listingTypeId: String?,
 //    @SerializedName("sanitized_title") val sanitizedTitle: String?,
@@ -34,21 +34,21 @@ data class MLProduct(
 //    @SerializedName("site_id") val siteId: String?,
 //    @SerializedName("category_id") val categoryId: String?,
 //    @SerializedName("domain_id") val domainId: String?,
-//    @SerializedName("thumbnail") val thumbnail: String?,
-//    @SerializedName("currency_id") val currencyId: String?,
+    @SerializedName("thumbnail") val thumbnail: String?,
+    @SerializedName("currency_id") val currencyId: String?,
 //    @SerializedName("order_backend") val orderBackend: Int?,
-//    @SerializedName("price") val price: Double?,
-//    @SerializedName("original_price") val originalPrice: Double?,
-//    @SerializedName("sale_price") val salePrice: Double?,
-//    @SerializedName("available_quantity") val availableQuantity: Int?,
+    @SerializedName("price") val price: Double?,
+    @SerializedName("original_price") val originalPrice: Double?,
+    @SerializedName("sale_price") val salePrice: Double?,
+    @SerializedName("available_quantity") val availableQuantity: Int,
 //    @SerializedName("official_store_id") val officialStoreId: Int?,
 //    @SerializedName("use_thumbnail_id") val useThumbnailId: Boolean?,
 //    @SerializedName("accepts_mercadopago") val acceptsMercadoPago: Boolean?,
-//    @SerializedName("shipping") val shipping: MLShipping?,
+    @SerializedName("shipping") val shipping: MLShippingResponse?,
 //    @SerializedName("stop_time") val stopTime: String?,
 //    @SerializedName("seller") val seller: MLSeller?,
 //    @SerializedName("attributes") val attributes: List<MLAttributes>?,
-//    @SerializedName("installments") val installments: MLInstallments?,
+    @SerializedName("installments") val installments: MLInstallmentsResponse?,
 //    @SerializedName("winner_item_id") val winnerItemId: String?,
 //    @SerializedName("catalog_listing") val catalogListing: Boolean?,
 //    @SerializedName("discounts") val discounts: List<Any>?,
@@ -57,7 +57,7 @@ data class MLProduct(
 //    @SerializedName("inventory_id") val inventoryId: String?
 )
 
-data class MLShipping(
+data class MLShippingResponse(
     @SerializedName("store_pick_up") val storePickUp: Boolean,
     @SerializedName("free_shipping") val freeShipping: Boolean,
     @SerializedName("logistic_type") val logisticType: String,
@@ -98,7 +98,7 @@ data class MLValue(
     @SerializedName("source") val source: Int
 )
 
-data class MLInstallments(
+data class MLInstallmentsResponse(
     @SerializedName("quantity") val quantity: Int,
     @SerializedName("amount") val amount: Double,
     @SerializedName("rate") val rate: Double,
@@ -109,12 +109,12 @@ data class MLDifferentialPricing(
     @SerializedName("id") val id: Int
 )
 
-data class MLSort(
+data class MLSortResponse(
     @SerializedName("id") val id: String,
     @SerializedName("name") val name: String
 )
 
-data class MLFilter(
+data class MLFilterResponse(
     @SerializedName("id") val id: String,
     @SerializedName("name") val name: String,
     @SerializedName("type") val type: String,
@@ -144,3 +144,4 @@ data class AvailableFilterValue(
     @SerializedName("name") val name: String,
     @SerializedName("results") val results: Int
 )
+
--- ml-android/app/src/main/java/com/vdelgado/ml/data/repository/MLSearchProductRepositoryImpl.kt
+++ ml-android/app/src/main/java/com/vdelgado/ml/data/repository/MLSearchProductRepositoryImpl.kt
@@ -1,17 +1,17 @@
-package com.vdelgado.ml.domain.repository.impl
+package com.vdelgado.ml.data.repository
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import com.vdelgado.ml.domain.model.MLProduct
 import com.vdelgado.ml.data.remote.MLServiceApi
 import com.vdelgado.ml.data.remote.SearchMLProductsPagingSource
+import com.vdelgado.ml.domain.model.MLProductFormatted
 import com.vdelgado.ml.domain.repository.MLSearchProductRepository
 import kotlinx.coroutines.flow.Flow
 
 class MLSearchProductRepositoryImpl(private val mlServiceApi: MLServiceApi) :
     MLSearchProductRepository {
-    override fun searchProduct(searchQuery: String): Flow<PagingData<MLProduct>> {
+    override fun searchProduct(searchQuery: String): Flow<PagingData<MLProductFormatted>> {
         return Pager(
             config = PagingConfig(
                 pageSize = 10,
