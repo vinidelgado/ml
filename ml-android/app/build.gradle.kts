defaf378ec671370ff91202b90c42d654a306ded
vdelgado <vinnygarciadelgado@gmail.com>
2024-07-30 16:40:57 -0300

    ⬆️ Upgrade dependencies

--- ml-android/app/build.gradle.kts
+++ ml-android/app/build.gradle.kts
@@ -21,7 +21,8 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "com.vdelgado.ml.webmock.MockTestRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -103,6 +104,16 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    androidTestImplementation(libs.okhttp.idling)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.mockwebserver)
+
+    androidTestImplementation(libs.hilt.android.testing)
+    kaptAndroidTest(libs.hilt.android.compiler)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    //Tracing
+    debugImplementation(libs.androidx.tracing)
 }
