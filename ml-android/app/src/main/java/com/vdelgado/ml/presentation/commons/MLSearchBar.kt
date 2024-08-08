9e05e13ab42d9e58943c3a64418a412b39e99c5a
vdelgado <vinnygarciadelgado@gmail.com>
2024-08-08 17:02:52 -0300

    ♻️ Separate TopBar and create a new strings to internacionalization

---
+++ ml-android/app/src/main/java/com/vdelgado/ml/presentation/commons/MLSearchBar.kt
@@ -0,0 +1,57 @@
+package com.vdelgado.ml.presentation.commons
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.vdelgado.ml.R
+import com.vdelgado.ml.presentation.home.HomeViewModel
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MLSearchBar(
+    onSearchClick: () -> Unit,
+    onQueryChange: (String) -> Unit,
+    state: HomeViewModel.HomeState,
+) {
+    SearchBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        placeholder = {
+            Text(text = stringResource(R.string.mlsearchbar_placeholder))
+        } ,
+        trailingIcon = {
+            if(state.searchQuery.isNotEmpty()) {
+                IconButton(
+                    onClick = {
+                        onQueryChange("")
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Close,
+                        contentDescription = stringResource(id = R.string.mlsearchbar_clean_icon_button_content_description),
+                    )
+                }
+            }
+        },
+        query = state.searchQuery,
+        onQueryChange = onQueryChange,
+        onSearch = {
+            onSearchClick()
+        },
+        active = false,
+        onActiveChange = {},
+        content = {},
+    )
+}
--- ml-android/app/src/main/java/com/vdelgado/ml/presentation/home/HomeScreen.kt
+++ ml-android/app/src/main/java/com/vdelgado/ml/presentation/home/HomeScreen.kt
@@ -1,9 +1,13 @@
 package com.vdelgado.ml.presentation.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,11 +22,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.vdelgado.ml.R
 import com.vdelgado.ml.presentation.commons.EmptyScreen
+import com.vdelgado.ml.presentation.commons.MLSearchBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,65 +42,66 @@ fun HomeScreen(
     event: (HomeViewModel.HomeEvent) -> Unit,
     navigateToDetails: (String) -> Unit
 ) {
-    var active by rememberSaveable { mutableStateOf(false) }
-    val keyboardController = LocalSoftwareKeyboardController.current
-    val focusManager = LocalFocusManager.current
+//    var active by rememberSaveable { mutableStateOf(false) }
+//    val keyboardController = LocalSoftwareKeyboardController.current
+//    val focusManager = LocalFocusManager.current
     Scaffold(
         topBar = {
-            SearchBar(
-                query = state.searchQuery,
-                onQueryChange = {
-                    event(HomeViewModel.HomeEvent.UpdateSearchQuery(it))
-                },
-                onSearch = {
-                    if (state.searchQuery.isNotEmpty()) {
-                        event(HomeViewModel.HomeEvent.SearchProduct)
-                    }
-                    active = false
-                    focusManager.clearFocus()
-                    keyboardController?.hide()
-                },
-                active = active,
-                onActiveChange = {
-                    if (state.searchQuery.isEmpty()) {
-                        active = it
-                    }
-                },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Default.Search,
-                        contentDescription = null,
-                        modifier.testTag("search-icon")
-                    )
-                },
-                placeholder = {
-                    Text(text = "Buscar no Mercado Livre")
-                },
-                trailingIcon = {
-                    if (active || state.searchQuery.isNotEmpty()) {
-                        IconButton(onClick = {
-                            if (state.searchQuery.isNotEmpty()) {
-                                event(HomeViewModel.HomeEvent.UpdateSearchQuery(""))
-                            } else {
-                                active = false
-                            }
-                        }, modifier.testTag("clear-icon")) {
-                            Icon(
-                                imageVector = Icons.Default.Close,
-                                contentDescription = "Fechar pesquisa",
-                                modifier = modifier.testTag("close-icon")
-                            )
-                        }
-                    }
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-
-            }
+            HomeAppBar(modifier = Modifier.fillMaxWidth(), state = state, event = event)
+//            SearchBar(
+//                query = state.searchQuery,
+//                onQueryChange = {
+//                    event(HomeViewModel.HomeEvent.UpdateSearchQuery(it))
+//                },
+//                onSearch = {
+//                    if (state.searchQuery.isNotEmpty()) {
+//                        event(HomeViewModel.HomeEvent.SearchProduct)
+//                    }
+//                    active = false
+//                    focusManager.clearFocus()
+//                    keyboardController?.hide()
+//                },
+//                active = active,
+//                onActiveChange = {
+//                    if (state.searchQuery.isEmpty()) {
+//                        active = it
+//                    }
+//                },
+//                leadingIcon = {
+//                    Icon(
+//                        imageVector = Icons.Default.Search,
+//                        contentDescription = null,
+//                        modifier.testTag("search-icon")
+//                    )
+//                },
+//                placeholder = {
+//                    Text(text = "Buscar no Mercado Livre")
+//                },
+//                trailingIcon = {
+//                    if (active || state.searchQuery.isNotEmpty()) {
+//                        IconButton(onClick = {
+//                            if (state.searchQuery.isNotEmpty()) {
+//                                event(HomeViewModel.HomeEvent.UpdateSearchQuery(""))
+//                            } else {
+//                                active = false
+//                            }
+//                        }, modifier.testTag("clear-icon")) {
+//                            Icon(
+//                                imageVector = Icons.Default.Close,
+//                                contentDescription = "Fechar pesquisa",
+//                                modifier = modifier.testTag("close-icon")
+//                            )
+//                        }
+//                    }
+//                },
+//                modifier = Modifier.fillMaxWidth()
+//            ) {
+//
+//            }
         },
         modifier = modifier.fillMaxSize()
     ) { innerPadding ->
-        if (state.searchQuery.isEmpty()) {
+        if (state.searchQuery.isEmpty() && state.products == null) {
             EmptyScreen(title = null, message = null)
         } else {
             state.products?.let { items ->
@@ -106,7 +117,25 @@ fun HomeScreen(
                 )
             }
         }
-
-
     }
+}
+
+@Composable
+private fun HomeAppBar(
+    modifier: Modifier = Modifier,
+    state: HomeViewModel.HomeState,
+    event: (HomeViewModel.HomeEvent) -> Unit,
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    MLSearchBar(
+        onCloseClick = { /*TODO*/ },
+        onSearchClick = {
+            event(HomeViewModel.HomeEvent.SearchProduct)
+            keyboardController?.hide()
+        },
+        onQueryChange = {
+            event(HomeViewModel.HomeEvent.UpdateSearchQuery(it))
+        },
+        state = state
+    )
 }
